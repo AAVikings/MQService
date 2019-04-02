19af6dab250276ede710e5fdbe5e166b9fadb58f
Matias <mati108@gmail.com>
2019-04-02 15:38:13 +0200

    Mocha implementation

--- orderLifeCicle/index.js
+++ orderLifeCicle/index.js
@@ -1,5 +1,37 @@
-const { MESSAGE_ENTITY, MESSAGE_TYPE, ORDER_CREATOR, ORDER_TYPE, ORDER_OWNER, ORDER_DIRECTION,
-  ORDER_STATUS, ORDER_EXIT_OUTCOME } = require("./types")
+const MESSAGE_ENTITY = {
+  SimulationExecutor: 'EX',
+  TradingCokpit: 'CO',
+  SimulationEngine: 'EN',
+  TradingAssistant: 'AS'
+}
+const MESSAGE_TYPE = {
+  HeartBeat: 'HBT',
+  OrderAuthorizationRequest: 'REQ',
+  OrderAuthorizationResponse: 'RES',
+  Order: 'ORD',
+  OrderUpdate: 'UPD'
+}
+const ORDER_CREATOR = { SimulationEngine: 'S', HumanTrader: 'H' }
+const ORDER_TYPE = { Market: 'M', Limit: 'L', Stop: 'S', }
+const ORDER_OWNER = { Node: 'N', Team: 'T', User: 'U', }
+const ORDER_DIRECTION = { Sell: 'Sell', Buy: 'Buy' }
+const ORDER_STATUS = {
+  Signaled: 'SIG', // The order has been created and is ready to be authorized
+  ManualAuthorized: 'MAU', // Authorized by the user manually
+  ManualNotAuthorized: 'MNA', // Rejected by the user manually
+  AutoAuthorized: 'AAU', // Authorize all as been selected by the user
+  AutoNotAuthorized: 'ANA', // Reject all as been selected by the user
+  Executing: 'EXE', // The order is efectively being executed on the exchange
+  Cancelled: 'CAN', // The order has been canceled
+  Filled: 'FIL',  // The order bas been completed on the exchange
+  PartiallyFilled: 'PRT', // The order is partially filled and is still on the exchange
+  Discarded: 'DIS', // Order discarded since there is already an order in place
+  Placed: 'PLA', // Order has been just been placed on the exchange
+  Rejected:'REJ', // Order has been rejected by the exchange due to some error
+  Undeliverable:'UND' // The order was not able to be delivered to the exchange
+}
+const ORDER_EXIT_OUTCOME = {  StopLoss: 'SL', TakeProfit: 'TP' }
+const ORDER_MARGIN_ENABLED = {  True: 1, False: 0 }
 
 function createRecord(messageId, from, to, messageType, messageDateTime, orderId, creator, orderDateTime,
   owner, exchange, market, marginEnabled, type, rate, stop, takeProfit, direction, size, status, sizeFilled, exitOutcome) {
@@ -29,6 +61,14 @@ function createRecord(messageId, from, to, messageType, messageDateTime, orderId
       exitOutcome
     ]
   ]
+}
+
+function createRecordFromObject(object) {
+
+  return createRecord(object.id, object.from, object.to, object.messageType, object.dateTime, object.order.id,
+    object.order.creator, object.order.dateTime, object.order.owner, object.order.exchange, object.order.market,
+    object.order.marginEnabled, object.order.type, object.order.rate, object.order.stop, object.order.takeProfit,
+    object.order.direction, object.order.size, object.order.status, object.order.sizeFilled, object.order.exitOutcome)
 }
 
 function getRecord(fileContent) {
@@ -77,7 +117,7 @@ function getExpandedRecord(fileContent) {
       owner: getKeyByValue(ORDER_OWNER, parsedFileContent[5][3]),
       exchange: parsedFileContent[5][4],
       market: parsedFileContent[5][5],
-      marginEnabled: parsedFileContent[5][6],
+      marginEnabled: getKeyByValue(ORDER_MARGIN_ENABLED, parsedFileContent[5][6]),
       type: getKeyByValue(ORDER_TYPE, parsedFileContent[5][7]),
       rate: parsedFileContent[5][8],
       stop: parsedFileContent[5][9],
@@ -103,5 +143,6 @@ function getKeyByValue(object, value) {
 }
 
 module.exports = {
-  createRecord, getRecord, getExpandedRecord
+  MESSAGE_ENTITY, MESSAGE_TYPE, ORDER_CREATOR, ORDER_TYPE, ORDER_OWNER, ORDER_DIRECTION, ORDER_STATUS, ORDER_EXIT_OUTCOME, ORDER_MARGIN_ENABLED,
+  createRecord, getRecord, getExpandedRecord, createRecordFromObject
 }
